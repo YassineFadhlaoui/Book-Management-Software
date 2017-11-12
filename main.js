82dba2fa4982a5b0e4706c8b5e9a6eeab514c96a
yassine <yassinefadhlaoui93@gmail.com>
2017-11-12 21:31:44 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const electron = require('electron')
+// Module to control application life.
+const app = electron.app
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow
+const url = require('url') 
+const path = require('path')  
+let $ = require('jquery');
+BrowserWindow.$ = $;
+let win  
+
+function createWindow() { 
+   win = new BrowserWindow({width: 900, height: 600}) 
+   win.loadURL(url.format ({ 
+      pathname: path.join(__dirname, 'index.html'), 
+      protocol: 'file:', 
+      slashes: true 
+   })) 
+}  
+
+app.on('ready', createWindow) 
---
+++ spawner.js
@@ -0,0 +1,3 @@
+const { spawn } = require('child_process');
+
+const child = spawn('electron',['test.js']);
