d6f44602c6f2c36e887f951de0e0b9872a1ebc5b
yassine <yassinefadhlaoui93@gmail.com>
2017-11-30 19:17:31 +0100

    adding menu bar

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
 const { app } = require('electron')
 const PDFWindow = require('electron-pdf-window')
- 
+const path = require('path')
 app.on('ready', () => {
   const win = new PDFWindow({
     width: 800,
-    height: 600
+    height: 600,
+    icon: path.join(__dirname, 'icon/viewer.png')
   })
  uri= process.argv[2]
   win.loadURL(uri)
